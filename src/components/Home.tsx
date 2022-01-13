e700eb515a0d0a0148844463a8ffb079208eaa46
uttampatro <uttampatro890@gmail.com>
2022-01-14 01:12:02 +0530

    logoutChanges

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import Home from '../pages/home/Index';
 import Header from './header/Header';
+import userServices from '../services/userServices';
+import { useHistory } from 'react-router-dom';
 
 function HomePage() {
+    const history = useHistory();
+    const User = localStorage.getItem('user');
+    const user = User ? JSON.parse(User) : undefined;
+    const logout = async () => {
+        try {
+            await userServices.logout();
+            history.push('/login');
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    if (!user) {
+        return <>Loading...</>;
+    }
     return (
         <div>
             <Header />
